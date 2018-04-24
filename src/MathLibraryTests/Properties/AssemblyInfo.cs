5f9bb93442d08212ea9d64ed88f8a5bf1017b91f
xberge04 <xberge04@fit.vutbr.cz>
2018-04-24 15:45:16 +0200

    Directory edited

--- src/MathLibraryTests/Properties/AssemblyInfo.cs
+++ src/MathLibraryTests/Properties/AssemblyInfo.cs
@@ -1,36 +1,20 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Obecné informace o sestavení se řídí přes následující 
-// sadu atributů. Změnou hodnot těchto atributů se upraví informace
-// přidružené k sestavení.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("MathLibraryTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("MathLibraryTests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
-// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
-// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
 [assembly: ComVisible(false)]
 
-// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
-[assembly: Guid("6e0e122f-50c6-479f-abb4-21c367e559b8")]
+[assembly: Guid("e39d2608-19f1-40cb-950c-cbb44191738e")]
 
-// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
-//
-//      Hlavní verze
-//      Podverze
-//      Číslo sestavení
-//      Revize
-//
-// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize pomocí *, jak je vidět níže:
-// pomocí zástupného znaku * takto:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
