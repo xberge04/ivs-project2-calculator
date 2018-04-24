15a79491c1d545ad2850658aff2348205dc3fd68
Vít Šebela <37558032+xsebel04@users.noreply.github.com>
2018-04-24 20:41:41 +0200

    added correct solution by specification

--- src/profiling.cs
+++ src/profiling.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    
+    /// <summary>
+    /// Program for profiling
+    /// </summary>
     class Program
     {
-        //Math_Library.Math Math;
-
+        
+        /// <summary>
+        /// Function for calculating standard deviation
+        /// </summary>
+        /// <param name="counter">number of input values</param>
+        /// <param name="input">list to store inputvalues</param>
         static void smerodatnaOdchylka(int counter, List<int> input)
         {
             Math_Library.Math math = new Math_Library.Math();
-            
-            //double smerodatnaOdchylka = 0;
+
             double soucet = 0;
             double prumer = 0;
             double temp = 0;
             double definitive = 0;
+
             for (int i = 0; i < counter; i++)
             {
                 soucet += input[i];
@@ -42,17 +48,14 @@ namespace Calculator
 
             int counter = 0;
             
-            System.IO.StreamReader file = new System.IO.StreamReader(@"c:\users\ivs\documents\visual studio 2015\Projects\profiling\profiling\TextFile3.txt");
             List<int> vstup = new List<int>();
 
             string line;
-            while ((line = file.ReadLine()) != null && line != "")
+            while ((line = Console.ReadLine()) != null && line != "")
             {
                 vstup.Add(int.Parse(line));
                 counter++;
             }
-            
-            file.Close();
 
             smerodatnaOdchylka(counter, vstup);
 
