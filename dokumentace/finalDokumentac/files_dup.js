173a597e8d578f0d92809448ff23eed1158c65e9
Vít Šebela <37558032+xsebel04@users.noreply.github.com>
2018-04-24 23:31:33 +0200

    add final documenatation

---
+++ dokumentace/finalDokumentac/files_dup.js
@@ -0,0 +1,27 @@
+var files_dup =
+[
+    [ "AdvancedMethodsTests.cs", "_advanced_methods_tests_8cs.html", [
+      [ "MathTests", "class_math_library_tests_1_1_math_tests.html", "class_math_library_tests_1_1_math_tests" ]
+    ] ],
+    [ "App.xaml.cs", "_app_8xaml_8cs.html", [
+      [ "App", "classcalculator_1_1_app.html", null ]
+    ] ],
+    [ "AssemblyInfo.cs", "_assembly_info_8cs.html", null ],
+    [ "BasicMethodsTests.cs", "_basic_methods_tests_8cs.html", [
+      [ "MathTests", "class_math_library_tests_1_1_math_tests.html", "class_math_library_tests_1_1_math_tests" ]
+    ] ],
+    [ "CalcBackend.cs", "_calc_backend_8cs.html", [
+      [ "CalcBackend", "classcalculator_1_1_calc_backend.html", "classcalculator_1_1_calc_backend" ]
+    ] ],
+    [ "MainWindow.xaml.cs", "_main_window_8xaml_8cs.html", [
+      [ "MainWindow", "classcalculator_1_1_main_window.html", "classcalculator_1_1_main_window" ]
+    ] ],
+    [ "Math.cs", "_math_8cs.html", [
+      [ "Math", "classcalculator_1_1_math___library_1_1_math.html", "classcalculator_1_1_math___library_1_1_math" ]
+    ] ],
+    [ "MathTests.cs", "_math_tests_8cs.html", [
+      [ "MathTests", "class_math_library_tests_1_1_math_tests.html", "class_math_library_tests_1_1_math_tests" ]
+    ] ],
+    [ "Resources.Designer.cs", "_resources_8_designer_8cs.html", null ],
+    [ "Settings.Designer.cs", "_settings_8_designer_8cs.html", null ]
+];
